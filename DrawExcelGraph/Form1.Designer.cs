ee61d0355bf01fb6b4b971e1b44d259912f91a67
masaya <e.cabbage63@gmail.com>
2015-07-01 20:14:39 +0900

    [add] made prototype
    enable to work

---
+++ DrawExcelGraph/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace MakeExcelChartsFromCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "read CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1.select target CSVs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(25, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2. Marge & Make Charts";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(27, 65);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(245, 71);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(241, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Done";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(153, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "saving xlsx completed";
+            this.label4.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(27, 202);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(245, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 262);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Merge CSVs & Make  Charts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DrawExcelGraph/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MakeExcelChartsFromCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<List<double>> li = new List<List<double>>();
+        List<string> fileName = new List<string>();
+
+        /// <summary>
+        /// Reading csv data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SetDataToList(ofd.FileNames);
+            }
+            label3.Visible = true;
+        }
+
+        /// <summary>
+        /// set csv data to list
+        /// </summary>
+        /// <param name="fileNames"></param>
+        private void SetDataToList(string[] fileNames)
+        {
+            string temp;
+            foreach (string fn in fileNames)
+            {
+                temp = Path.GetFileName(fn);
+                textBox1.Text += temp + Environment.NewLine;
+                string[] s = temp.Split('.');
+                fileName.Add(s[0]);
+                ReadCSV(fn);
+            }
+        }
+
+        /// <summary>
+        /// read CSV data
+        /// </summary>
+        /// <param name="fileName"></param>
+        private void ReadCSV(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName, Encoding.GetEncoding("Shift_JIS"));
+            li.Add(new List<double>());
+            while (sr.EndOfStream == false)
+            {
+                string line = sr.ReadLine();
+                li[li.Count - 1].Add(double.Parse(line));
+            }
+        }
+
+        /// <summary>
+        /// Create Excel sheet
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(li.Count);
+            for (int i = 0; i < li.Count; i++)
+            {
+                Console.WriteLine("li[" + i + "]:" + li[i].Count);
+            }
+
+            makeGraph();
+            label4.Visible = true;
+        }
+
+        void makeGraph()
+        {
+            // Open Excel API
+            using (var excelApplication = new NetOffice.ExcelApi.Application())
+            {
+                // Add workbook
+                var workBook = excelApplication.Workbooks.Add();
+                workBook.Worksheets.Add();
+                // set worksheet to set data
+                var dataSheet = (NetOffice.ExcelApi.Worksheet)workBook.Worksheets[1];
+                dataSheet.Name = "Data";
+                // set worksheet to set chart
+                var chartSheet = (NetOffice.ExcelApi.Worksheet)workBook.Worksheets[2];
+                chartSheet.Name = "Charts";
+
+                // input data
+                progressBar1.Maximum = li.Count;
+                for (int i = 0; i < li.Count; i++)
+                {
+                    dataSheet.Cells[1, i + 1].Value = fileName[i];
+                    for (int j = 0; j < li[i].Count; j++)
+                    {
+                        dataSheet.Cells[j + 2, i + 1].Value = li[i][j];
+                    }
+
+                    var chart = ((NetOffice.ExcelApi.ChartObjects)chartSheet.ChartObjects()).Add(0, 200*i, 350, 200);
+                    chart.Chart.ChartType = NetOffice.ExcelApi.Enums.XlChartType.xlLine;
+                    chart.Chart.SetSourceData(dataSheet.Range(dataSheet.Cells[1, i + 1], dataSheet.Cells[li[i].Count, i + 1]));
+
+                    progressBar1.PerformStep();
+                }
+
+                workBook.SaveAs(sfd());
+                excelApplication.Quit();
+            }
+        }
+
+        string sfd()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "*.xlsx";
+            sfd.Filter = "Excel Book(*.xlsx;*.xls)|*.xlsx;*.xls|All Type(*.*)|*.*";
+            sfd.FilterIndex = 1;
+            sfd.Title = "Select directory to save file";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                return sfd.FileName;
+            }
+
+            return "";
+        }
+    }
+}
